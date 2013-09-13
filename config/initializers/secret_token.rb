5a818fe5b4a523869038475666d6b9b80b35aa39
Marco De Canal <marco.decanal@gmail.com>
2013-09-13 17:19:01 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mathemagic::Application.config.secret_token = '45c53e88384f0d2c70aab348b137fc5d7583ebc0c2995fb97cc6325dfb04bbd79dd77a1841d1aff522a7f25cbc4a58ea895073c165dd205857e9ae7afd82e4f6'
