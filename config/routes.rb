03fec352bde855cb3c6984f54f59ea6e22587347
Marco De Canal <marco.decanal@gmail.com>
2013-09-14 13:38:51 +0200

    Deleted index.html and added bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Mathemagic::Application.routes.draw do
+#  get "pages/about"
+
+ # get "pages/home"
+
+#  get "pages/contact"
+    
+    match '/about', :to => 'pages#about'
+    match '/contact', :to => 'pages#contact'
+    root :to => 'pages#home'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
